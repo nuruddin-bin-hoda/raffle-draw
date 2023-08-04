4d33deb5926094b9c54523514468d62a3fdfba13
Nuruddin <nuruddinbinhoda@gmail.com>
2023-08-04 19:38:46 +0600

    add router handeler

--- app/router.js
+++ app/router.js
@@ -1,6 +1,9 @@
 // external import
 const router = require("express").Router();
 
+// app route handeler
+router.use("/api/v1/tickets", require("../routes/tickets"));
+
 // app health check
 router.get("/health", (_req, res) => {
   res.status(200).json({
---
+++ routes/tickets.js
@@ -0,0 +1,8 @@
+// route handeler
+const router = require("express").Router();
+
+router.get("/", (req, res) => {
+  res.send("Hello world!");
+});
+
+module.exports = router;
